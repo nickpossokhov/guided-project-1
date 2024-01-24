bd59cdc7df925473d1f0994496c073ef5be93807
Nick <npossokh@travelers.com>
2024-01-24 10:45:16 -0500

    added and formatted general info for films

--- guided_project_1/film.js
+++ guided_project_1/film.js
@@ -1,6 +1,24 @@
+let nameH1;
+let episodeIdH2;
+let openingCrawlP;
+let releaseDateSpan;
+let directorSpan;
+let producerSpan;
+let filmsUl;
+let planetsUl;
 const baseUrl = "https://swapi2.azurewebsites.net/api/films";
 
+
 addEventListener('DOMContentLoaded', () => {
+  nameH1 = document.querySelector('h1#name');
+  episodeIdH2 = document.querySelector('h2#episode_id');
+  openingCrawlP = document.querySelector('p#opening_crawl');
+  releaseDateSpan = document.querySelector('span#release_date');
+  directorSpan = document.querySelector('span#director');
+  producerSpan = document.querySelector('span#producer');
+  filmsUl = document.querySelector('#films>ul');
+  planetsUl = document.querySelector('#planets>ul');
+
   const sp = new URLSearchParams(window.location.search);
   const id = sp.get('id');
 
@@ -24,6 +42,13 @@ async function getFilm(id) {
 
 function renderFilm(film) {
   document.title = film.title;
+
+  nameH1.innerHTML = film.title;
+  episodeIdH2.innerHTML = `Episode ${film.episode_id}`;
+  openingCrawlP.innerHTML = film.opening_crawl;
+  releaseDateSpan.innerHTML = film.release_date
+  directorSpan.innerHTML = film.director;
+  producerSpan.innerHTML = film.producer;
 }
 
 async function fetchFilm(id) {
