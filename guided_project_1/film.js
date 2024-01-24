c6e9c0f40d88eabd7ae01d2a5e72935c6a0d69ab
Nick <npossokh@travelers.com>
2024-01-24 10:55:20 -0500

    added planet and character information for films

--- guided_project_1/film.js
+++ guided_project_1/film.js
@@ -4,7 +4,7 @@ let openingCrawlP;
 let releaseDateSpan;
 let directorSpan;
 let producerSpan;
-let filmsUl;
+let charactersUl;
 let planetsUl;
 const baseUrl = "https://swapi2.azurewebsites.net/api/films";
 
@@ -16,7 +16,7 @@ addEventListener('DOMContentLoaded', () => {
   releaseDateSpan = document.querySelector('span#release_date');
   directorSpan = document.querySelector('span#director');
   producerSpan = document.querySelector('span#producer');
-  filmsUl = document.querySelector('#films>ul');
+  charactersUl = document.querySelector('#characters>ul');
   planetsUl = document.querySelector('#planets>ul');
 
   const sp = new URLSearchParams(window.location.search);
@@ -36,19 +36,25 @@ async function getFilm(id) {
     console.error(ex);
   }
 
-  console.log(film);
   renderFilm(film);
 }
 
 function renderFilm(film) {
   document.title = film.title;
-
   nameH1.innerHTML = film.title;
   episodeIdH2.innerHTML = `Episode ${film.episode_id}`;
   openingCrawlP.innerHTML = film.opening_crawl;
   releaseDateSpan.innerHTML = film.release_date
   directorSpan.innerHTML = film.director;
   producerSpan.innerHTML = film.producer;
+
+  let planets = film.planets;
+  planets = planets.map(planet => `<li><a href="/planet.html?id=${planet.id}">${planet.name}</li>`);
+  planetsUl.innerHTML = planets.join('');
+
+  let characters = film.characters;
+  characters = characters.map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`);
+  charactersUl.innerHTML = characters.join('');
 }
 
 async function fetchFilm(id) {
